6585329ecb329a550ff0a6cca81c5c4f9cabe82b
JasonThorne <Jason@JAY-PC>
2016-11-22 00:58:27 +0000

    displaying users location (WIP)

--- MobileAppProj/MobileAppProj/MainPage.xaml.cs
+++ MobileAppProj/MobileAppProj/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,19 +33,48 @@ namespace MobileAppProj
         }
 
 
-       
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            // Specify Galway location.
-            BasicGeoposition GalwayPosition = new BasicGeoposition() { Latitude = 53.270962, Longitude = -9.062691};
-            Geopoint cityCenter = new Geopoint(GalwayPosition);
 
-            // Set the map to Galway location.
-            MyMap.Center = cityCenter;
-            MyMap.ZoomLevel = 14;
-            MyMap.LandmarksVisible = true;
+            /*
+             // Specify Galway location.
+             BasicGeoposition GalwayPosition = new BasicGeoposition() { Latitude = 53.270962, Longitude = -9.062691};
+             Geopoint cityCenter = new Geopoint(GalwayPosition);
+
+             // Set the map to Galway location.
+             MyMap.Center = cityCenter;
+             MyMap.ZoomLevel = 14;
+             MyMap.LandmarksVisible = true;
+             */
+
+
+          
+            // Set users current location.
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                    // Get users current location.
+                    Geolocator geolocator = new Geolocator();
+                    Geoposition pos = await geolocator.GetGeopositionAsync();
+                    Geopoint myLocation = pos.Coordinate.Point;
+
+                    // Set the map's location.
+                    MyMap.Center = myLocation;
+                    MyMap.ZoomLevel = 14;
+                    MyMap.LandmarksVisible = true;
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+
+                    //Ask user to change location settings if access is denied
+                    bool result = await Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-location"));
+                    break;
+            }
+
+
         }
-
-
     }
 }

e892f689b280c3f7632f5befd4e046e6a89914dd
JasonThorne <Jason@JAY-PC>
2016-11-28 21:29:38 +0000

    message box now showing time till arrival

--- MobileAppProj/MobileAppProj/MainPage.xaml.cs
+++ MobileAppProj/MobileAppProj/MainPage.xaml.cs
@@ -36,26 +36,12 @@ namespace MobileAppProj
     {
 
         private BusStops[] busStopData;
-       // private DepartureTimes departureTimeData;
-        //private List<MapIconObj> mapIconList = new List<MapIconObj>();
-
-
-        //ArrayList tempArray = new ArrayList(); //DELETE++++++++++++++++++
-
+      
 
         public MainPage()
         {
             this.InitializeComponent();
         }
-
-        /*
-        public class MapIconObj
-        {
-            public MapIcon mapIcon { get; set; }
-            public int stop_id { get; set; }
-        }
-        */
-
 
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -64,18 +50,6 @@ namespace MobileAppProj
             makeMap();
             await populateMap();
 
-            /*
-            string stop_ref = "";
-
-            //collect stop_refs 
-            for (int i = 0; i < busStopData.Length; i++)
-            {
-                stop_ref = busStopData[i].stop_ref;
-                tempArray.Add(stop_ref);
-                textBoxTest1.Text += " " + tempArray[i]; //test
-            }
-            */
-
         }
 
 
@@ -83,42 +57,42 @@ namespace MobileAppProj
         {
 
 
-            // Set users current location.
+            // Set users current location
             var accessStatus = await Geolocator.RequestAccessAsync();
             switch (accessStatus)
             {
                 case GeolocationAccessStatus.Allowed:
 
 
-                    // Get users current location.
-                    Geolocator geolocator = new Geolocator();
-                    Geoposition pos = await geolocator.GetGeopositionAsync();
-                    Geopoint myLocation = pos.Coordinate.Point;
+                // Get users current location
+                Geolocator geolocator = new Geolocator();
+                Geoposition pos = await geolocator.GetGeopositionAsync();
+                Geopoint myLocation = pos.Coordinate.Point;
 
-                    // Create an icon for user
-                    MapIcon userIcon = new MapIcon();
-                    userIcon.Location = myLocation;
-                    userIcon.NormalizedAnchorPoint = new Point(0.5, 1.0);
-                    userIcon.Title = "You are here";
-                    userIcon.ZIndex = 0;
-                    userIcon.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/userIcon.png"));
+                // Create an icon for user
+                MapIcon userIcon = new MapIcon();
+                userIcon.Location = myLocation;
+                userIcon.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                userIcon.Title = "You are here";
+                userIcon.ZIndex = 0;
+                userIcon.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/userIcon.png"));
 
 
-                    // Add the icon to the map.
-                    MyMap.MapElements.Add(userIcon);
+                // Add the icon to the map
+                MyMap.MapElements.Add(userIcon);
 
-                    // Set the map's location.
-                    MyMap.Center = myLocation;
-                    MyMap.ZoomLevel = 14;
-                    MyMap.LandmarksVisible = true;
+                // Set the map's location
+                MyMap.Center = myLocation;
+                MyMap.ZoomLevel = 16; //14
+                MyMap.LandmarksVisible = true;
 
-                    break;
+                break;
 
-                case GeolocationAccessStatus.Denied: //++++++++++++++++++++++++++++++THIS NEEDS HANDLED!! 
+            case GeolocationAccessStatus.Denied: //++++++++++++++++++++++++++++++THIS NEEDS HANDLED!! 
 
-                    //Ask user to change location settings.
-                    bool result = await Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-location"));
-                    break;
+                //Ask user to change location settings.
+                bool result = await Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-location"));
+                break;
             }
         }
 
@@ -138,11 +112,7 @@ namespace MobileAppProj
 
                 //create map icon
                 MapIcon tempMapIcon = new MapIcon();
-                //var tempButton = new Button();
-                //tempButton.Content = "TEST"; //////////////////////////REMOVE LATER!! (& make button invisible)
-                //tempButton.Visibility = System.Windows.Visibility.Hidden; //---------------NOT WORKING! 
-                //this.MyMap.Children.Add(tempButton);
-
+                
 
                 //find location for icon
                 BasicGeoposition iconPosition = new BasicGeoposition()
@@ -151,30 +121,12 @@ namespace MobileAppProj
                     Longitude = busStopData[i].longitude
                 };
 
-                /*
-                //find location for button
-                var buttonPosition = new Geopoint(new BasicGeoposition()
-                {
-                    Latitude = busStopData[i].latitude,
-                    Longitude = busStopData[i].longitude
-                });
-                //MapControl.SetLocation(tempButton, buttonPosition);
-                */
-                //add button to map
-                //MapControl.SetNormalizedAnchorPoint(tempButton, new Point(0.5, 1.0));
-
-                //add values to obj
-
-                //add obj to list
-                ///mapIconList.Add(tempMapIconObj);
-
-
+ 
                 //define icon
                 Geopoint iconPoint = new Geopoint(iconPosition);
                 tempMapIcon.Location = iconPoint;
                 tempMapIcon.NormalizedAnchorPoint = new Point(0.5, 1.0);
                 tempMapIcon.ZIndex = 0;
-                //tempMapIcon.Title = busStopData[i].stop_id.ToString();
                 tempMapIcon.Title = busStopData[i].long_name;
                 //tempMapIcon.Title = busStopData[i].stop_ref;
                 /*
@@ -188,25 +140,13 @@ namespace MobileAppProj
 
             }
 
-
-
-            /*
-            //find departure time data using stop_refs 
-            for (int j=0; j<tempArray.Count; j++)
-            { 
-                departureTimeData = await GetDepartureTimes.API_Call(tempArray[j].ToString());
-               textBoxTest2.Text += " " + departureTimeData.stop.stop_ref;
-               // textBoxTest2.Text += " " + departureTimeData.times[j].depart_timestamp; //++index out of bounds
-            }
-          */
-
             return busStopData;
 
         }
 
 
 
-
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++DELETE!!
         private void test1_Click(object sender, RoutedEventArgs e)
         {
 
@@ -216,6 +156,8 @@ namespace MobileAppProj
             //var data = GetBusStops.GetBusStopData();
             //textBoxTest1.Text = data.ToString();
         }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++DELETE!!
+
 
 
         private void mapIcon_Click(MapControl sender, MapElementClickEventArgs args)
@@ -226,36 +168,55 @@ namespace MobileAppProj
             //Debug.WriteLine("Icon clicked with ID: " + clickedIcon.Title); //test 
             Debug.WriteLine("Icon clicked with Long Name: " + clickedIcon.Title); //test 
 
-            if (clickedIcon.Title == "You are here")
-            {
-                showMessage(clickedIcon.Title);
-            }
-            else
-            {
+            string long_name = clickedIcon.Title;
 
+            /*
+            switch (long_name)
+            {
+                case "You are here":
+                    showMessage(long_name);
+                    break;
+                default:
+                    string stop_ref = "";
+
+                    //loop through busStopData
+                    for (int i = 0; i < busStopData.Length; i++)
+                    {
+                        //find stop_ref
+                        if (long_name == busStopData[i].long_name)
+                        {
+                            stop_ref = busStopData[i].stop_ref;
+                            break;
+                        }
+                    }
+
+                    makeDepartureTimes(stop_ref);
+                    break;
+            }*/
+           
+
+            
+            if (!long_name.Contains("You are here"))
+            {
                 string stop_ref = "";
 
                 //loop through busStopData
                 for (int i = 0; i < busStopData.Length; i++)
                 {
                     //find stop_ref
-                    if (clickedIcon.Title == busStopData[i].long_name)
+                    if (long_name == busStopData[i].long_name)
                     {
                         stop_ref = busStopData[i].stop_ref;
                         break;
                     }
                 }
 
-
-                //makeDepartureTimes(clickedIcon.Title, stop_ref);
-                //makeDepartureTimes(clickedIcon.Title);
-
-                //showDepartureTimes(clickedIcon.Title); ///test
                 makeDepartureTimes(stop_ref);
-
             }
-
-
+            else
+            {
+                showMessage(long_name);
+            }
 
         }
 
@@ -263,21 +224,38 @@ namespace MobileAppProj
         private async void makeDepartureTimes(string stop_ref)
         {
 
-            DepartureTimes departureTimeData = await GetDepartureTimes.API_Call(stop_ref);
+            //ProgressRing progRing = new ProgressRing();
 
-           string message = string.Format("Route" + " " + "Destination" + " " + "Time" + "\n\n");
-           
+           // progRing.IsActive = true; //++++++++++++++++++++++++++++++++++++++++++++++++++++++NOT WORKING?? 
+            DepartureTimes departureTimeData = await GetDepartureTimes.API_Call(stop_ref);
+           // progRing.IsActive = false;
+
+            string message = string.Format("Route" + " | " + "Destination" +  " | " + "Time" + "\n\n");
+            ///string depart_timestamp = "";
+            DateTime nextBus;
+            DateTime currentTime;
+            TimeSpan timeUntillNextBus;
+            
 
             //loop through list of departure times
             for (int i = 0; i < departureTimeData.times.Count; i++)
             {
+                //find due time
+                //depart_timestamp = departureTimeData.times[i].depart_timestamp;
+                nextBus = DateTime.Parse(departureTimeData.times[i].depart_timestamp);
+                currentTime = DateTime.UtcNow;
+                timeUntillNextBus = nextBus.Subtract(currentTime);
 
-                message += departureTimeData.times[i].depart_timestamp + "\n\n";
+                 //append string
+                 message += string.Format("{0}" + " | " + "{1}" + " | " + "{2} hours" + " {3} minutes" + "\n\n", departureTimeData.times[i].timetable_id, departureTimeData.times[i].display_name, timeUntillNextBus.Hours, timeUntillNextBus.Minutes);
             }
 
             showMessage(message);
 
         }
+
+
+
 
         private async void showMessage(string message)
         {
@@ -286,4 +264,5 @@ namespace MobileAppProj
         }
 
     }
+
 }
